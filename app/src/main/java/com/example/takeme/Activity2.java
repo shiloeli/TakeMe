7bad18b54cc5d57b1734f5c109019c977f120072
shiloa3@gmail.com <shiloa3@gmail.com>
2021-12-03 09:08:02 +0200

    add login option

--- app/src/main/java/com/example/takeme/Activity2.java
+++ app/src/main/java/com/example/takeme/Activity2.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,8 +22,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Activity2 extends AppCompatActivity {
     private FirebaseAuth mAuth;
     Button button;
-    EditText emailAddress;
-    EditText password;
+    EditText emailAddress, password, name, lastName, phone, cv, carType, password2;
+    RadioButton male, female;
+    Switch driver, passenger;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +33,50 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_2);
 
         emailAddress = (EditText) findViewById(R.id.emailTxt);
-
         password = (EditText) findViewById(R.id.Passwordtxt);
+        password2 = (EditText) findViewById(R.id.reg_pass2);
+        name = (EditText) findViewById(R.id.Nametxt);
+        lastName = (EditText) findViewById(R.id.reg_lastName);
+        phone = (EditText) findViewById(R.id.phoneNumbre);
+        cv = (EditText) findViewById(R.id.reg_cv);
+        carType = (EditText) findViewById(R.id.reg_car);
+        male = (RadioButton) findViewById(R.id.reg_male);
+        female = (RadioButton) findViewById(R.id.reg_female);
+        driver = (Switch) findViewById(R.id.reg_driver);
+        passenger = (Switch) findViewById(R.id.reg_passenger);
 
         button = (Button) findViewById(R.id.button_connect);
 
         mAuth = FirebaseAuth.getInstance();
 
+//        if(mAuth.getCurrentUser() != null){
+//            startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class));
+//            finish();
+//        }
+
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
                 String txtEmail = emailAddress.getText().toString();
                 String pass = password.getText().toString();
 
                 if(TextUtils.isEmpty(txtEmail)){
-                    emailAddress.setError("Email is Required.");
+                    emailAddress.setError("נדרשת כתובת אימייל");
                     return;
                 }
                 if(TextUtils.isEmpty(pass)){
-                    password.setError("Password is required.");
+                    password.setError("נדרשת סיסמה");
                 }
 
                 mAuth.createUserWithEmailAndPassword(txtEmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
                      if(task.isSuccessful()){
-                         Toast.makeText(Activity2.this, "User Created.", Toast.LENGTH_SHORT).show();
+                         Toast.makeText(Activity2.this, "משתמש נוצר", Toast.LENGTH_SHORT).show();
                          startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class));
                      }else{
-                        Toast.makeText(Activity2.this, "Error ! "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-
+                        Toast.makeText(Activity2.this, "שגיאה!"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                      }
                     }
                 });
--- app/src/main/java/com/example/takeme/MainActivity.java
+++ app/src/main/java/com/example/takeme/MainActivity.java
@@ -1,25 +1,56 @@
 package com.example.takeme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    EditText txtPassword, txtName;
+    Button buttLog;
 
-    EditText txtPassword;
-    EditText txtName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtName=( EditText)findViewById(R.id.txtName);
-        txtPassword=( EditText)findViewById(R.id.txtPassword);
-        String StringtxtName=txtName.getText().toString();
-        String StringtxtPass=txtPassword.getText().toString();
 
+        txtName=( EditText)findViewById(R.id.textEmail);
+        txtPassword=( EditText)findViewById(R.id.txtPassword);
+        buttLog = (Button) findViewById(R.id.btnSend);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        buttLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String StringtxtEmail=txtName.getText().toString();
+                String StringtxtPass=txtPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(StringtxtEmail, StringtxtPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "התחברות בוצעה בהצלחה", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class));
+                        }else{
+                            Toast.makeText(MainActivity.this, "שגיאה!"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
     }
 
     public void onClickReg(View view) {
@@ -28,9 +59,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intenet);
     }
 
-    public void onClickSend(View view) {
-        Intent intenet=new Intent(MainActivity.this,DriverOrTrempist.class);
-        Bundle b=new Bundle();
-        startActivity(intenet);
-    }
+
 }
