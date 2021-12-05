b2dddd9434a6861c96a731d2b774181fbf3f7d48
avihugoren <73278156+avihugoren@users.noreply.github.com>
2021-12-05 19:49:11 +0200

    v1

--- app/src/main/java/com/example/takeme/Activity2.java
+++ app/src/main/java/com/example/takeme/Activity2.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -18,8 +20,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Activity2 extends AppCompatActivity {
+    private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
     Button button;
     EditText emailAddress, password, name, lastName, phone, cv, carType, password2;
@@ -44,9 +48,8 @@ public class Activity2 extends AppCompatActivity {
         female = (RadioButton) findViewById(R.id.reg_female);
         driver = (Switch) findViewById(R.id.reg_driver);
         passenger = (Switch) findViewById(R.id.reg_passenger);
-
         button = (Button) findViewById(R.id.button_connect);
-
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
 
 //        if(mAuth.getCurrentUser() != null){
@@ -60,20 +63,62 @@ public class Activity2 extends AppCompatActivity {
             public void onClick(View view) {
                 String txtEmail = emailAddress.getText().toString();
                 String pass = password.getText().toString();
+                String txtCv = cv.getText().toString();
+                String txtName = name.getText().toString();
+                String txtLastName = lastName.getText().toString();
+                String txtPhone = phone.getText().toString();
+                String txtCarType=carType.getText().toString();
 
-                if(TextUtils.isEmpty(txtEmail)){
+                if(TextUtils.isEmpty(txtEmail))
+                {
                     emailAddress.setError("נדרשת כתובת אימייל");
                     return;
                 }
-                if(TextUtils.isEmpty(pass)){
+                if(TextUtils.isEmpty(pass))
+                {
                     password.setError("נדרשת סיסמה");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtName))
+                {
+                    password.setError("נדרש שם");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtLastName))
+                {
+                    password.setError("נדרש שם משפחה");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtPhone))
+                {
+                    password.setError("נדרש מספר פלאפון");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtCarType))
+                {
+                    password.setError("נדרש מספר רכב");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtPhone))
+                {
+                    password.setError("נדרש מספר תעודת זהות");
+                    return;
+                }
+                if(!driver.isChecked() && !passenger.isChecked())
+                {
+                    driver.setError("נא לבחור אחת מהאופציות");
+                    passenger.setError("נא לבחור אחת מהאופציות");
+                    return;
                 }
 
                 mAuth.createUserWithEmailAndPassword(txtEmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                      if(task.isSuccessful()){
+                         User user=new User(txtCv,txtName,txtEmail,txtLastName,txtPhone,txtCarType,male.isChecked(),driver.isChecked(),passenger.isChecked());
+//                         mDatabase.child("users").child(txtCv).setValue(user);
                          Toast.makeText(Activity2.this, "משתמש נוצר", Toast.LENGTH_SHORT).show();
+
                          startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class));
                      }else{
                         Toast.makeText(Activity2.this, "שגיאה!"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/takeme/User.java
@@ -0,0 +1,40 @@
+package com.example.takeme;
+
+public class User {
+    private String cv;
+    private String userName;
+    private String email;
+    private String name;
+    private String lastName;
+    private String phone;
+    private String carType;
+    private Boolean gender;
+    private Boolean driver;
+    private Boolean passenger;
+
+
+
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.userName = username;
+        this.email = email;
+    }
+    public User(String cv,String name,String email,String lastName,String phone,String carType,Boolean gender,Boolean driver,Boolean passenger)
+    {
+        this.cv=cv;
+        this.name=name;
+        this.lastName=lastName;
+        this.phone=phone;
+        this.carType=carType;
+        this.gender=gender;
+        this.driver=driver;
+        this.passenger=passenger;
+        this.email=email;
+
+    }
+
+}
