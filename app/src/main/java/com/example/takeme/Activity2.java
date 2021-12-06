8652e44bcd9b39cc02ff4441ccf601aabcfbfddf
noa haim <noa.202.r@gmail.com>
2021-12-06 16:05:09 +0200

    update

--- app/src/main/java/com/example/takeme/Activity2.java
+++ app/src/main/java/com/example/takeme/Activity2.java
@@ -6,25 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
+
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.annotation.Documented;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Activity2 extends AppCompatActivity {
+    public static final String TAG = TAG;
     private FirebaseAuth mAuth;
     Button button;
-    EditText emailAddress, password, name, lastName, phone, cv, carType, password2;
+    EditText emailAddress, password, name, lastName, phone, id, carType, password2;
     RadioButton male, female;
     Switch driver, passenger;
+    FirebaseFirestore fStore;
+    String userID;
 
 
     @Override
@@ -38,7 +49,7 @@ public class Activity2 extends AppCompatActivity {
         name = (EditText) findViewById(R.id.Nametxt);
         lastName = (EditText) findViewById(R.id.reg_lastName);
         phone = (EditText) findViewById(R.id.phoneNumbre);
-        cv = (EditText) findViewById(R.id.reg_cv);
+        id = (EditText) findViewById(R.id.reg_cv);
         carType = (EditText) findViewById(R.id.reg_car);
         male = (RadioButton) findViewById(R.id.reg_male);
         female = (RadioButton) findViewById(R.id.reg_female);
@@ -60,6 +71,11 @@ public class Activity2 extends AppCompatActivity {
             public void onClick(View view) {
                 String txtEmail = emailAddress.getText().toString();
                 String pass = password.getText().toString();
+                String txtID = id.getText().toString();
+                String txtName = name.getText().toString();
+                String txtLastName = lastName.getText().toString();
+                String txtPhone = phone.getText().toString();
+                String txtCarType=carType.getText().toString();
 
                 if(TextUtils.isEmpty(txtEmail)){
                     emailAddress.setError("נדרשת כתובת אימייל");
@@ -67,13 +83,62 @@ public class Activity2 extends AppCompatActivity {
                 }
                 if(TextUtils.isEmpty(pass)){
                     password.setError("נדרשת סיסמה");
+                    return;
                 }
+                if(TextUtils.isEmpty(txtName))
+                {
+                    name.setError("נדרש שם");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtLastName))
+                {
+                    lastName.setError("נדרש שם משפחה");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtPhone))
+                {
+                    phone.setError("נדרש מספר פלאפון");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtCarType)&&driver.isChecked())
+                {
+                    carType.setError("נדרש סוג רכב");
+                    return;
+                }
+                if(TextUtils.isEmpty(txtID))
+                {
+                    id.setError("נדרש מספר תעודת זהות");
+                    return;
+                }
+                if(!driver.isChecked() && !passenger.isChecked())
+                {
+                    driver.setError("נא לבחור אחת מהאופציות");
+                    passenger.setError("נא לבחור אחת מהאופציות");
+                    return;
+                }
+
 
                 mAuth.createUserWithEmailAndPassword(txtEmail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                      if(task.isSuccessful()){
                          Toast.makeText(Activity2.this, "משתמש נוצר", Toast.LENGTH_SHORT).show();
+                         userID=mAuth.getCurrentUser().getUid();
+                         DocumentReference documentReference=fStore.collection("users").document(userID);
+                         Map<String,Object> user=new HashMap<>();
+                         user.put("name",txtName);
+                         user.put("email",txtEmail);
+                         user.put("password",pass);
+                         user.put("lastName",txtLastName);
+                         user.put("id",txtID);
+                         user.put("phone",txtPhone);
+                         user.put("carType",txtCarType);
+                         documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                             @Override
+                             public void onSuccess(Void unused) {
+                                 Log.d(TAG, "onSuccess: user Profile is created for "+userID);
+                             }
+                         })
                          startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class));
                      }else{
                         Toast.makeText(Activity2.this, "שגיאה!"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
