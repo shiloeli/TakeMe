62f60444fafb7cf9a32cec331e92e3c75202edfc
tehilabadi <tehilaqwer@gmail.com>
2021-12-18 22:55:26 +0200

    DataBase commit

--- app/src/main/java/com/example/takeme/DataBase.java
+++ app/src/main/java/com/example/takeme/DataBase.java
@@ -15,14 +15,17 @@ public class DataBase {
     private static FirebaseFirestore fStore = FirebaseFirestore.getInstance();
     private static DocumentReference documentReference;
 
-
-
+    public static String getID(){
+        return mAuth.getCurrentUser().getUid();
+    }
     public static Task<AuthResult> CreateUser(String email, String password){
         return mAuth.createUserWithEmailAndPassword(email,password);
-
+    }
+    public static Task<AuthResult> SignIn(String email, String password){
+        return mAuth.signInWithEmailAndPassword(email,password);
     }
     public static void createUser(String collection, String txtName,String txtLastName,String txtEmail,String txtPhone,String txtID, boolean male){
-        String ID = mAuth.getCurrentUser().getUid();
+        String ID = getID();
         documentReference = fStore.collection(collection).document(ID);
         User user = new User(txtName, txtLastName, txtEmail, txtPhone, txtID, male);
         documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -31,10 +34,10 @@ public class DataBase {
                 Log.d(TAG, "onSuccess: user profile is create for" + ID);
             }
         });
-
     }
+
     public static void createTremp(String collection, String txtSrcCity,String txtDestCity,String txtDay,String txtHour,String txtDate,String txtSeatsNum){
-        String userDbId = mAuth.getCurrentUser().getUid();
+        String userDbId = getID();
         documentReference = fStore.collection(collection).document(userDbId);
         Tremp tremp = new Tremp(txtSrcCity, txtDestCity, txtDay, txtHour, txtDate, txtSeatsNum);
         documentReference.set(tremp).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -43,10 +46,9 @@ public class DataBase {
                 Log.d(TAG, "onSuccess: Tremp is create for" + userDbId);
             }
         });
-
     }
     public static void createDriver(String collection, String name,String lastName,String email,String phone,String id,int StringNumberCar,String StringTypeCar,String StringColor,boolean male){
-        String ID = mAuth.getCurrentUser().getUid();
+        String ID = getID();
         documentReference = fStore.collection(collection).document(ID);
         Driver userDriver = new Driver(name, lastName, email, phone, id, male ,StringNumberCar,StringTypeCar,StringColor);
         documentReference.set(userDriver).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -55,8 +57,6 @@ public class DataBase {
                 Log.d(TAG, "onSuccess: user profile is create for" + ID);
             }
         });
-
     }
-
 }
 
--- app/src/main/java/com/example/takeme/MainActivity.java
+++ app/src/main/java/com/example/takeme/MainActivity.java
@@ -17,21 +17,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth mAuth;
     EditText txtPassword, txtName;
     Button buttLog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         txtName=( EditText)findViewById(R.id.textEmail);
         txtPassword=( EditText)findViewById(R.id.txtPassword);
         buttLog = (Button)findViewById(R.id.btnSend);
-
-        mAuth = FirebaseAuth.getInstance();
 
         buttLog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +44,12 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                mAuth.signInWithEmailAndPassword(StringtxtEmail, StringtxtPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                DataBase.SignIn(StringtxtEmail, StringtxtPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(MainActivity.this, "התחברות בוצעה בהצלחה", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class).putExtra("UID",mAuth.getUid()));
+                            startActivity(new Intent(getApplicationContext(), DriverOrTrempist.class).putExtra("UID",DataBase.getID()));
                         }else{
                             Toast.makeText(MainActivity.this, "שגיאה!"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
@@ -65,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickReg(View view) {
-        Intent intenet=new Intent(MainActivity.this, UserRegister.class).putExtra("UID",mAuth.getUid());
+        Intent intenet=new Intent(MainActivity.this, UserRegister.class).putExtra("UID",DataBase.getID());
         startActivity(intenet);
     }
 
